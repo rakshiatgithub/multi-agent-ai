799aac6b7517176683471c247919db96260c475c
Rakshitha <150587543+rakshiatgithub@users.noreply.github.com>
2026-07-14 16:21:49 +0530

    Enhance researcher function with LLM integration
    Refactor researcher function to include a research plan and utilize ChatOllama for generating responses.

--- agents/researcher.py
+++ agents/researcher.py
@@ -1,7 +1,31 @@
 from tools.retriever import get_retriever
+from langchain_community.chat_models import ChatOllama
 
 retriever = get_retriever()
 
-def researcher(query):
+llm = ChatOllama(model="llama3")
+
+def researcher(query, plan):
+
     docs = retriever.invoke(query)
-    return "\n".join([doc.page_content for doc in docs])
+
+    context = "\n".join(
+        doc.page_content
+        for doc in docs
+    )
+
+    prompt=f"""
+You are an AI Research Agent.
+
+Research Plan:
+
+{plan}
+
+Retrieved Documents:
+
+{context}
+
+Answer the user's question using only the retrieved context.
+"""
+
+    return llm.invoke(prompt).content
