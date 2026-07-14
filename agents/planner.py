ad6edbcd7b081305fd9b515dde14cfd824d761aa
Rakshitha <150587543+rakshiatgithub@users.noreply.github.com>
2026-07-14 16:21:07 +0530

    Update planner.py

--- agents/planner.py
+++ agents/planner.py
@@ -1,11 +1,30 @@
 from langchain_openai import ChatOpenAI
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0
+)
 
 def planner(query):
-    response = llm.invoke(f"Break this task into clear steps:\n{query}")
-    return response.content
+
+    prompt=f"""
+You are an expert AI Planner.
+
+For the question:
+
+{query}
+
+Generate:
+
+1. Goal
+2. Important keywords
+3. Research strategy
+4. Expected final output
+
+Respond in markdown.
+"""
+
+    return llm.invoke(prompt).content
